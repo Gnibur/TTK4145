f8f7325ae795e22fa7e39490a5e4815e1686c6ee
Rubin Gerritsen <rubin.gerritsen@gmail.com>
2015-01-20 21:10:58 +0100

    safeData.go. No longer a data race on variable i. Using a variablemanager with a 'for, select' loop

--- Exercise2/safeData.go
+++ Exercise2/safeData.go
@@ -2,28 +2,55 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"time"
 )
 
-var i int = 0
 
+var increaseChannel = make(chan bool)
+var decreaseChannel = make(chan bool)
+
+var i int = 0
 func main(){
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	go thread1()
-	go thread2()
-	time.Sleep(1000*time.Millisecond)
+
+
+	var done = make (chan bool)
+
+	go variableManager()
+	go thread1(done)
+	go thread2(done)
+	
+
+	for j := 0; j < 2; j++ {
+		<- done
+	}
+	
 	fmt.Printf("i is now %v\n", i)
 }
 
-func thread1(){
-	for j := 0; j < 1000000; j++ {
-		i++	
+func variableManager(){
+	for {
+	    select {
+	  	case <-increaseChannel:
+	    	 i++
+	    case <-decreaseChannel:
+	    	 i--
+		}
 	}
+
 }
 
-func thread2(){
-	for j := 0; j < 1000000; j++ {
-		i--
+func thread1(doneChannel chan<- bool){
+	for j := 0; j < 10000; j++ {
+	 
+		increaseChannel <- true
 	}
+
+	doneChannel <- true	
+}
+
+func thread2(doneChannel chan<- bool){
+	for j := 0; j < 10000; j++ {
+		decreaseChannel <- true
+	}
+
+	doneChannel <- true
 }
