792a13dfeee500c99dc8ace1ca92544767008f8b
hakontei <hakontei@stud.ntnu.no>
2015-02-18 09:54:13 +0100

    merged both recv and send, primary and backup

--- Exercise6/exercise6.c
+++ Exercise6/exercise6.c
@@ -66,13 +66,41 @@ int recvUdpBroadcast(int targetPort, char buf[], int buflen)
     return 0;
 }
 
+int sendUdpToAddr(char addr[], char targetPort[], char buf[], int buflen)
+{
+	int sockfd;
+	struct addrinfo hints, *servinfo;
+	
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if (getaddrinfo(addr, targetPort, &hints, &servinfo) != 0)
+	{
+		perror("talker: gettaddr");
+		return 0;
+	}
+	if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
+	{
+		perror("talker: socket");
+		return 0;
+	}
+	if (sendto(sockfd, buf, buflen, 0, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+	{
+		perror("talker: send");
+		return 0;
+	}
+	freeaddrinfo(servinfo);
+	return 1;
+}
+
 
 int main(int argc, char *argv[])
 {
   runAsBackup();
   printf("Now it runs as primary\n");
   spawnNewBackup();
-  //runAsPrimary();
+  runAsPrimary();
 }
 
 
@@ -88,19 +116,32 @@ void runAsBackup(void)
     
     if (lastReceiveTime + TIMEOUT_LIMIT < time(0))
       break;
-
-    printf("Receiving...\n");
+    	printf("Receiving...\n");
     if (recvUdpBroadcast(BROADCAST_PORT, buf, BUFFER_LENGTH)) {
 
       printf("Received broadcast\n");
       int temp = readImAliveMessage(buf, BUFFER_LENGTH);
 
       if (temp != -1){
-	currentNumber = temp;
-	lastReceiveTime = time(0);
+				currentNumber = temp;
+				lastReceiveTime = time(0);
       }
     }
   }
+}
+
+void runAsPrimary(void)
+{
+	int x = 0;
+	char msg[512];
+	while (1)
+	{
+		sprintf(msg, "%d", x);
+		sendUdpToAddr("127.0.0.1", "2000", msg, 512);
+		printf("Counting: %d\n", x);
+		x = x + 1;
+		sleep(2);
+	}
 }
 
 int readImAliveMessage(char buf[], int buflen)
