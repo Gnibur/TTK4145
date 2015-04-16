a4f13633ceab867a43f25fa91c4ac7f0c0de87c8
Håkon Teigen Lund <hakontei@stud.ntnu.no>
2015-04-16 15:56:07 +0200

    Made a sceleton for the new ordermanager

---
+++ Project/driver/OrderManager_new.cpp
@@ -0,0 +1,28 @@
+#include "OrderManager_new.h" // Change to OrderManager.h
+
+void OrderManager::newOrder(int floor, order_direction_t direction)
+{
+}
+
+void OrderManager::clearOrder(Order order)
+{
+}
+
+bool OrderManager::hasOrderOnFloor(int floor)
+{
+}
+
+motor_direction_t OrderManager::getNextDirection(int floor, motor_direction_t lastDirection)
+{
+
+}
+
+int OrderManager::getCost(int floor)
+{
+
+}
+
+int mergeOrdersWith(OrderList orders)
+{
+
+}
